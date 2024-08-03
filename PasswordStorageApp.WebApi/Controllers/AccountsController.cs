332f617d41826ce6ff60d89b6af6d42a04bf274f
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-08-03 17:14:13 +0300

    Add project files.

---
+++ PasswordStorageApp.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasswordStorageApp.WebApi.Dtos;
+using PasswordStorageApp.WebApi.Models;
+using PasswordStorageApp.WebApi.Persistence;
+
+namespace PasswordStorageApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var accounts = FakeDbContext.Accounts.ToList();
+            return Ok(accounts);
+        }
+        [HttpPost]
+        public IActionResult Create(AccountCreateDto newAccount)
+        {
+            var account = newAccount.ToAccount();
+            FakeDbContext.Accounts.Add(account);
+            return Ok(new { data = account.Id, message = "The account was added successfully" });
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult Update(Guid id, AccountUpdateDto updateDto)
+        {
+            if (id != updateDto.Id)
+            {
+                return BadRequest("The id in the url does not match the id in the body");
+            }
+            var account = FakeDbContext.Accounts.FirstOrDefault(ac => ac.Id == id);
+            var updatedAccount = updateDto.ToAccount(account);
+            var index = FakeDbContext.Accounts.FindIndex(ac => ac.Id == id);
+            FakeDbContext.Accounts[index] = updatedAccount;
+            return Ok(new { data = account.Id, message = "The account was uptaded successfully" });
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            var account = FakeDbContext.Accounts.FirstOrDefault(ac => ac.Id == id);
+            if (account is null)
+            {
+                return NotFound();
+            }
+            return Ok(account);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult Remove(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest("id is not valid. Please do not send empty guids for god sake!");
+            }
+
+            var account = FakeDbContext.Accounts.FirstOrDefault(ac => ac.Id == id);
+            if (account is null)
+            {
+                return NotFound();
+            }
+            FakeDbContext.Accounts.Remove(account);
+            return NoContent();
+        }
+    }
+}
---
+++ PasswordStorageApp.WebApi/Controllers/PasswordsController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordStorageApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordsController : ControllerBase
+    {
+        [HttpGet] // Veriyi almak istediğimizi belirtiyoruz.
+        public IActionResult GetAll()
+        {
+            var passwords = _passwords;
+            return Ok(passwords);
+        }
+
+        [HttpGet("{passwordId}")]
+        public IActionResult GetById(string passwordId)
+        {
+            var password = _passwords.FirstOrDefault(x => x == passwordId);
+            if (string.IsNullOrEmpty(password))
+            {
+                return NotFound();
+            }
+            return Ok(password);
+        }
+
+        [HttpDelete("{passwordId}")]
+        public IActionResult Remove(string passwordId)
+        {
+            var password = _passwords.FirstOrDefault(x => x == passwordId);
+            if (string.IsNullOrEmpty(password))
+            {
+                return NotFound();
+            }
+            _passwords.Remove(password);
+            return NoContent();
+        }
+
+
+        private static readonly List<string> _passwords = new()
+        {
+            "123456",
+            "password",
+            "123456789",
+            "12345678",
+            "12345",
+            "1234567",
+            "1234567890",
+            "qwerty",
+            "abc123",
+            "111111",
+            "123123",
+            "admin",
+            "letmein",
+            "welcome",
+            "monkey",
+            "password1",
+            "1234",
+            "password",
+            "superman",
+            "iloveyou",
+            "123456a",
+            "trustno1",
+            "1234567",
+            "sunshine",
+            "master",
+            "123123",
+            "welcome",
+            "shadow",
+            "ashley",
+            "football",
+            "jesus",
+            "michael",
+            "ninja",
+            "mustang",
+            "password1",
+            "p@ssw0rd",
+            "hello",
+            "charlie",
+            "aa123456",
+            "donald",
+            "password",
+            "qazwsx"
+        };
+    }
+
+}
+
+/*
+Controller bir hastane resepsiyonu gibi düşünülebilir. Yani gelen istekleri karşılar ve ilgili işlemleri yapar.
+GET isteği ile veri alınır.
+POST isteği ile veri eklenir.
+PUT isteği ile veri güncellenir.
+DELETE isteği ile veri silinir.
+PATCH isteği ile veri kısmen güncellenir. (Örneğin bir kullanıcının sadece ismi güncellenir.)
+
+IActionResult: Bir işlem sonucunda geriye döndürülecek veriyi belirtir. Örneğin bir metot bir string döndürüyorsa IActionResult yerine string yazılabilir.
+
+HTTPS Kodları: 
+200 OK: İstek başarılı bir şekilde gerçekleşti.
+201 Created: İstek başarılı bir şekilde gerçekleşti ve yeni bir kaynak oluşturuldu.
+204 No Content: İstek başarılı bir şekilde gerçekleşti ancak geriye herhangi bir içerik döndürülmedi.
+400 Bad Request: İstek hatalı.
+401 Unauthorized: Kullanıcı kimlik doğrulaması gerekiyor.
+403 Forbidden: Kullanıcı yetkili değil.
+404 Not Found: İstek yapılan kaynak bulunamadı.
+405 Method Not Allowed: İstek yapılan metot desteklenmiyor.
+409 Conflict: İstek yapılan işlem ile ilgili bir çakışma var.
+500 Internal Server Error: Sunucu hatası.
+
+
+_ : private bir alanı belirtir. Bu alan sadece bu sınıf içerisinde kullanılabilir.
+
+FirstOrDefault: Bir koleksiyon içerisinde belirtilen koşula uyan ilk elemanı getirir. Eğer koşula uyan bir eleman yoksa null döner.
+ */
